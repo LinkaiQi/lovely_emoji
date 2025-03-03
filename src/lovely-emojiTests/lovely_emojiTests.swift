4ee51fe772689160fe40a425d2a8dd32c0fa6725
Linkai Qi <lqi.linkai@gmail.com>
2025-03-02 20:22:31 -0800

    Create Project

---
+++ src/lovely-emojiTests/lovely_emojiTests.swift
@@ -0,0 +1,17 @@
+//
+//  lovely_emojiTests.swift
+//  lovely-emojiTests
+//
+//  Created by Linkai Qi on 2025-03-02.
+//
+
+import Testing
+@testable import lovely_emoji
+
+struct lovely_emojiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ src/lovely-emojiUITests/lovely_emojiUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  lovely_emojiUITestsLaunchTests.swift
+//  lovely-emojiUITests
+//
+//  Created by Linkai Qi on 2025-03-02.
+//
+
+import XCTest
+
+final class lovely_emojiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
